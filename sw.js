9ed71962fb19eae6ef3d6324b97578f42cc93407
nextleveltienda <nextleveltienda.nl@gmail.com>
2026-06-10 19:26:34 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'nl-tienda-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'nl-tienda-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
